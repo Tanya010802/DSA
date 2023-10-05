4e4c4e7a15781a3d635aedd326345de725536dd7
Tanya010802 <91143210+Tanya010802@users.noreply.github.com>
2023-10-05 23:13:58 +0530

    Update string_1.cpp

---
+++ strings/string_1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+    // your code goes here
+    string s;
+    cin>>s;
+    if(s.length()==1)
+    cout<<"YES";
+    else{
+        cout<<"no";
+    }
+}
