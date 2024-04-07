5e3bcd1bf65c71c412c256dafb6bb2aaaacc9bff
Tanya010802 <91143210+Tanya010802@users.noreply.github.com>
2024-04-08 00:38:40 +0530

    Create remove_ele.cpp

---
+++ Arrays/remove_ele.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n = nums.size();
+        int count = 0;
+        int index = 0;
+        int i = 0;
+        while(index < n){
+            
+            if(nums[i] != val){
+                count++;
+                index++;
+                i = i+1;
+                
+            }
+            else{
+                nums.erase(nums.begin() + i);
+                index++;
+            }
+        }
+         return count;
+    }
+};
